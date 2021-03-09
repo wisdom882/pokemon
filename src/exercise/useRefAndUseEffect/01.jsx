8d8b90c0834386d6144b6274d55b53dbbe0cad73
wisdom <miabiyewisdom@gmail.com>
2021-03-08 20:26:27 -0800

    vanillaTilit

--- src/exercise/useRefAndUseEffect/01.jsx
+++ src/exercise/useRefAndUseEffect/01.jsx
@@ -11,6 +11,7 @@ import file from './01.md'
 function Tilt({children}){
 
     // 🐨 create a ref here with React.useRef()
+    const tiltRef = React.useRef();
 
   // 🐨 add a `React.useEffect` callback here and use VanillaTilt to make your
   // div look fancy.
@@ -22,6 +23,18 @@ function Tilt({children}){
   //   glare: true,
   //   'max-glare': 0.5,
   // })
+    React.useEffect(() => {
+      const tiltNode = tiltRef.current
+       VanillaTilt.init(tiltNode, {
+        max: 25,
+        speed: 400,
+         glare: true,
+        'max-glare': 0.5,
+       })
+
+       return () => tiltNode.vanillaTilt.destroy()
+
+    },[])
   //
   // 💰 Don't forget to return a cleanup function. VanillaTilt.init will add an
   // object to your DOM node to cleanup:
@@ -33,7 +46,7 @@ function Tilt({children}){
   // 🐨 add the `ref` prop to the `tilt-root` div here:
 
     return (
-        <div className="tilt-root">
+        <div className="tilt-root" ref = {tiltRef}>
             <div className="tilt-child">{children}</div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './solutions/useEffectHTTPrequests/01'
+import App from './exercise/useRefAndUseEffect/01'
 import reportWebVitals from './reportWebVitals';
 
 import 'prismjs/themes/prism.css'; /* or your own custom theme */
